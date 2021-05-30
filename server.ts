4ed86271970a4e34dd11322a185370e477528bfc
Ramki A <kvraamkeydev@gmail.com>
2021-05-30 21:17:25 +0530

    cors enabled

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import Router from "./src/routes/index.ts";
 
 const app = new Application();
 const port = 8080;
+
+app.use(oakCors());
 
 app.use(Router.prefix("/solr").routes());
 app.use(Router.allowedMethods());
