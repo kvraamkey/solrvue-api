4dce9da9ec188973ab939d380c30c06bf1d84137
Ramki A <kvraamkeydev@gmail.com>
2021-05-31 14:05:27 +0530

    handling all route request

---
+++ router.ts
@@ -0,0 +1,23 @@
+import { Router, RouterContext, soxa } from "./deps.ts";
+import { to, response } from "./helpers.ts";
+
+const router = new Router();
+
+router.post("/:controller?", async ({ response: res, request: req, params }: RouterContext) => {
+    if (!params.controller || !req.headers.get("solr_action")) {
+        res.body = response("A valid [controller] or [action] is required.");
+    } else {
+        try {
+            const Authorization = btoa(req.headers.get("solr_username") + ":" + req.headers.get("solr_password"));
+            soxa.defaults.baseURL = req.headers.get("solr_url");
+            soxa.defaults.headers.common["Authorization"] = `Basic ${Authorization}`;
+            const [err, module] = await to(import(`./handlers/${params.controller}/${req.headers.get("solr_action")}.ts`));
+            if (err || module) response(err);
+            res.body = await module.default();
+        } catch (error) {
+            res.body = response(error.message);
+        }
+    }
+});
+
+export default router;
