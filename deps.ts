12bcf44b4eb5c75e422bcba87653dcccdaba27f4
Ramki A <kvraamkeydev@gmail.com>
2021-05-31 14:02:23 +0530

    all deps moved

---
+++ deps.ts
@@ -0,0 +1,8 @@
+import "https://deno.land/x/dotenv/load.ts";
+
+export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export type { RouterContext, Context } from "https://deno.land/x/oak/mod.ts";
+
+export { oakCors } from "https://deno.land/x/cors/mod.ts";
+export { soxa } from "https://deno.land/x/soxa/mod.ts";
+export { parse } from "https://deno.land/std/flags/mod.ts";
